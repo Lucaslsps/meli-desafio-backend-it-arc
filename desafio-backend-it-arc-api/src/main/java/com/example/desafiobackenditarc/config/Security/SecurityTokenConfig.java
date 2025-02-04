81ed5bed650a3e28d8393c0b5f76c38a8584b7b5
Lucas Peixoto <lucas.peixoto@luizalabs.com>
2025-02-03 22:47:45 -0300

    feat: adding jwt auth

---
+++ desafio-backend-it-arc-api/src/main/java/com/example/desafiobackenditarc/config/Security/SecurityTokenConfig.java
@@ -0,0 +1,45 @@
+package com.example.desafiobackenditarc.config.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtConfig jwtConfig;
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+
+        http
+                .csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new DefaultAuthenticationEntryPoint())
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        "/actuator/health",
+                        "/actuator/info",
+                        "/swagger-resources/**",
+                        "/v2/api-docs/**",
+                        "/swagger-ui.html",
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**"
+                ).permitAll()
+                .anyRequest()
+                .authenticated();
+
+        http.addFilter(new JwtTokenAuthenticationFilter(authenticationManager(), jwtConfig));
+
+    }
+}
